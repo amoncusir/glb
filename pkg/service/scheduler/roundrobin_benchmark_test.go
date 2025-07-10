84f75c2d4d9148dc145f6f5aea4f2e563ba02a16
Aran Moncusi Ramirez <aran.moncusi@orbidi.com>
2025-07-10 18:06:09 +0200

    test(benchmark): Add a benchmark to compare both roundrobin implementations

---
+++ pkg/service/scheduler/roundrobin_benchmark_test.go
@@ -0,0 +1,78 @@
+package scheduler_test
+
+import (
+	"amoncusir/example/pkg/service/instance"
+	"amoncusir/example/pkg/service/scheduler"
+	"amoncusir/example/pkg/tools/unres"
+	"fmt"
+	"testing"
+)
+
+func generateInstances(n int) []instance.Instance {
+	ins := make([]instance.Instance, n)
+
+	for i := range ins {
+		u, _ := unres.ExtractFromUri(fmt.Sprintf("tcp://%d", i))
+		ins[i] = instance.New(u)
+	}
+
+	return ins
+}
+
+func BenchmarkRoundRobinOneThread(b *testing.B) {
+	ins := generateInstances(50)
+
+	b.SetParallelism(1)
+
+	b.Run("Atomic Int counter implementation", func(b *testing.B) {
+		sh := scheduler.RoundRobin()
+		for b.Loop() {
+			sh.Select(ins)
+		}
+	})
+
+	b.Run("Buffered Channel counter implementation with double of instance size", func(b *testing.B) {
+		sh := scheduler.AsyncRoundRobin(100)
+		for b.Loop() {
+			sh.Select(ins)
+		}
+	})
+
+	b.Run("Buffered Channel counter implementation with half of instance size", func(b *testing.B) {
+		sh := scheduler.AsyncRoundRobin(25)
+		for b.Loop() {
+			sh.Select(ins)
+		}
+	})
+}
+
+func BenchmarkRoundRobinParallel(b *testing.B) {
+	ins := generateInstances(50)
+
+	b.Run("Atomic Int counter implementation", func(b *testing.B) {
+		sh := scheduler.RoundRobin()
+		b.RunParallel(func(p *testing.PB) {
+			for p.Next() {
+				sh.Select(ins)
+			}
+		})
+	})
+
+	b.Run("Buffered Channel counter implementation with double of instance size", func(b *testing.B) {
+		sh := scheduler.AsyncRoundRobin(100)
+		b.RunParallel(func(p *testing.PB) {
+			for p.Next() {
+				sh.Select(ins)
+			}
+		})
+	})
+
+	b.Run("Buffered Channel counter implementation with half of instance size", func(b *testing.B) {
+		sh := scheduler.AsyncRoundRobin(25)
+		b.RunParallel(func(p *testing.PB) {
+			for p.Next() {
+				sh.Select(ins)
+			}
+		})
+	})
+}
