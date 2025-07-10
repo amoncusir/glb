447045308728fb7355fdbd2cbd39ed52dcfdf2e7
Aran Moncusi Ramirez <aran.moncusi@orbidi.com>
2025-07-10 22:52:51 +0200

    feat(unsafe): Ratelimit filter using the TokenBucket algorithm.
    Isn't safe because it's tested yet. TBD in next commits!

---
+++ pkg/router/filter/ratelimit.go
@@ -0,0 +1,59 @@
+package filter
+
+import (
+	"amoncusir/example/pkg/types"
+	"context"
+	"errors"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	PRESSURE_SIZE = 16                 // 2^16 = 65536 values
+	RATE_SIZE     = 64 - PRESSURE_SIZE // 2^48 = 281474976710656 values
+)
+
+func TBRatelimit() PreFilter {
+	return &rtbRatelimit{}
+}
+
+// TokenBucket implementation to filter by reate limit
+// uint64 type prevent casting and type conversion during the compute
+type rtbRatelimit struct {
+	capacity        uint64
+	compressionRate uint64 // in milliseconds
+	buckets         *sync.Map
+}
+
+// Accept implements PreFilter.
+func (r *rtbRatelimit) Accept(ctx context.Context, req types.RequestConn) error {
+	b := r.getDataByReq(req)
+	now := uint64(time.Now().UnixMilli() >> RATE_SIZE)
+
+	lb := b.Load()
+
+	lp := lb >> RATE_SIZE
+	elapsed := now - uint64(lb<<PRESSURE_SIZE)
+	compression := elapsed * r.compressionRate
+
+	pressure := min(r.capacity, lp+compression)
+
+	if pressure <= 0 {
+		// Not Allowed also not saved as modifier
+		return errors.New("blocked petition by rate limit")
+	}
+
+	lb = ((pressure - 1) << RATE_SIZE) & now
+
+	b.Store(lb)
+
+	return nil
+}
+
+// Uses the first (BE) 16 bits for the counter and last 48 bits for the last acceded time
+func (r *rtbRatelimit) getDataByReq(req types.RequestConn) *atomic.Uint64 {
+	// TODO: The current req.RemoteAddr().String() get the IP + Port of the remote. Must be changed!
+	b, _ := r.buckets.LoadOrStore(req.RemoteAddr().String(), &atomic.Uint64{})
+	return b.(*atomic.Uint64)
+}
