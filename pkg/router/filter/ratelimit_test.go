91fe68c9d1b18b16ea89f692114a5e20d15c0df5
Aran Moncusi Ramirez <aran.moncusi@orbidi.com>
2025-07-13 20:45:07 +0200

    test: Add test to check the desviation error of the implementation in a high parallelization environments

--- pkg/router/filter/ratelimit_test.go
+++ pkg/router/filter/ratelimit_test.go
@@ -3,6 +3,7 @@ package filter
 import (
 	"amoncusir/example/test/mock"
 	"context"
+	"sync"
 	"testing"
 	"time"
 
@@ -50,4 +51,121 @@ func TestRateLimitDenyedAndAcceptBeforeTime(t *testing.T) {
 
 	assert.Empty(rl.Accept(ctx, req))
 	assert.NotEmpty(rl.Accept(ctx, req))
+}
+
+func TestRateLimitReccoverAllCapacity(t *testing.T) {
+	assert := assert.New(t)
+	ctrl := gomock.NewController(t)
+	req := mock.NewMockRequestConn(ctrl)
+	ctx := context.Background()
+
+	req.EXPECT().RemoteIp().Return("0.0.0.0").AnyTimes()
+
+	rl := newtbRatelimit(10, 1_000/10)
+
+	for range 10 {
+		err := rl.Accept(ctx, req)
+		assert.Empty(err)
+	}
+
+	assert.NotEmpty(rl.Accept(ctx, req))
+
+	time.Sleep(1 * time.Second)
+
+	for range 10 {
+		err := rl.Accept(ctx, req)
+		assert.Empty(err)
+	}
+
+	assert.NotEmpty(rl.Accept(ctx, req))
+}
+
+func TestRateLimitOnConcurrentCallsNoDrainCapacity(t *testing.T) {
+	iterations := 10
+	assert := assert.New(t)
+	ctrl := gomock.NewController(t)
+	req := mock.NewMockRequestConn(ctrl)
+	ctx := context.Background()
+
+	req.EXPECT().RemoteIp().Return("0.0.0.0").AnyTimes()
+
+	rl := newtbRatelimit(10, 100/10) // 100 accepts every second
+
+	wg := &sync.WaitGroup{}
+
+	callFn := func() {
+		defer wg.Done()
+
+		for range 10 {
+			err := rl.Accept(ctx, req)
+			assert.Empty(err)
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+
+	wg.Add(iterations) // Perform 100 calls in one second
+	for range iterations {
+		go callFn()
+	}
+
+	wg.Wait()
+}
+
+func TestRateLimitOnConcurrentCallsDrainCapacity(t *testing.T) {
+	parallelization := 10_000
+	windowTime := 10
+
+	initialBudget := 10
+	petitions := 100
+
+	acceptableDeviation := 0.995
+
+	assert := assert.New(t)
+	ctrl := gomock.NewController(t)
+	req := mock.NewMockRequestConn(ctrl)
+	ctx := context.Background()
+
+	req.EXPECT().RemoteIp().Return("0.0.0.0").AnyTimes()
+
+	rl := newtbRatelimit(uint16(initialBudget), uint(windowTime/initialBudget)) // 100 accepts every second
+
+	errs := make(chan error)
+	wg := &sync.WaitGroup{}
+
+	callFn := func() {
+		defer wg.Done()
+		for range petitions {
+			errs <- rl.Accept(ctx, req)
+			time.Sleep(time.Duration(windowTime) * time.Millisecond)
+		}
+	}
+
+	go func() {
+		wg.Wait()
+		close(errs)
+	}()
+
+	wg.Add(parallelization) // Perform 100 calls in one second
+	for range parallelization {
+		go callFn()
+	}
+
+	collectedErrors := []error{}
+
+	for err := range errs {
+		if err != nil {
+			collectedErrors = append(collectedErrors, err)
+		}
+	}
+
+	// This algoritm has eventual consistence and more than one request could be accepted.
+	// An acceptable deviation is less then
+	totalCaluledErrors := parallelization*petitions - (petitions * initialBudget)
+	deviation := 1.0 - acceptableDeviation
+	errorsDeviation := int(float64(totalCaluledErrors) * deviation)
+
+	t.Logf("Total errors: %d over calculed: %d and deviation of %d", len(collectedErrors), totalCaluledErrors, errorsDeviation)
+
+	assert.LessOrEqual(len(collectedErrors), totalCaluledErrors+errorsDeviation)
+	assert.GreaterOrEqual(len(collectedErrors), totalCaluledErrors-errorsDeviation)
 }
